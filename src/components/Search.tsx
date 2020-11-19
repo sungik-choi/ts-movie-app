c60f1b0f4923acc187176650c017371401e43dad
최성익 <tjddlr92@gmail.com>
2020-11-20 05:26:26 +0900

    Refactor: 비제어 컴포넌트 다시 제어 컴포넌트로 변경
    - 굳이 ref를 사용할 필요 없다고 판단함

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 
 interface ISearchProps {
   search: (url: string) => void;
 }
 
 const Search = ({ search }: ISearchProps): JSX.Element => {
-  const inputEl = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
 
   const callSearchFunction = (e: React.MouseEvent<HTMLInputElement>) => {
     e.preventDefault();
-    if (inputEl.current !== null) {
-      search(inputEl.current.value);
-      inputEl.current.value = '';
-    }
+    search(value);
+    setValue('');
   };
 
   return (
     <form className="search">
-      <input ref={inputEl} type="text" />
+      <input value={value} onChange={handleInputChange} type="text" />
       <input onClick={callSearchFunction} type="submit" value="SEARCH" />
     </form>
   );
